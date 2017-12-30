22caf13989ea54f7f972e0a273d223759bf09729
Simon Brun <sb.simonbrun@gmail.com>
2017-12-30 19:34:39 +0100

    reference data handling permutation matrix between basis and ref basis start with cohomology

--- TestSage.py
+++ TestSage.py
@@ -5,16 +5,6 @@ import logging
 
 G = graphs.WheelGraph(5)
 
-for (j, ee) in enumerate(G.edges(labels=False)):
-    a, b = ee
-    G.set_edge_label(a, b, j)
 
-print(G.edges())
+G.plot().save('./save.png')
 
-G.merge_vertices([3,4])
-
-print(G.edges())
-
-
-print(type(G)==type(Graph()))
-print(type(Graph()))
