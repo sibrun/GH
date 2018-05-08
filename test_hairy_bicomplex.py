d1ff9d61f13dd79971761c5f707c2ab83a8bbceb
Simon Brun <sb.simonbrun@gmail.com>
2018-05-08 16:12:34 +0200

    Bug: Bicomplex don't square to zero

--- test_hairy_bicomplex.py
+++ test_hairy_bicomplex.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     even_e = False
     even_h = False
-    deg_range = range(5, 12)
-    h_min_range = range(-9, -1)
+    deg_range = range(5, 11)
+    h_min_range = range(-11, -1)
 
     gc = HGBC.HairyBiGC(deg_range, h_min_range, even_e, even_h)
 
