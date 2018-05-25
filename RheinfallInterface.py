71a8ede6a931dfdb75c2122f1fdd5d841739349e
Simon Brun <sb.simonbrun@gmail.com>
2018-05-25 13:44:52 +0200

    rheinfall rank

--- RheinfallInterface.py
+++ RheinfallInterface.py
@@ -8,9 +8,7 @@ def rank(rheinfall_option, matrix_file):
         raise ValueError('Possible options for rheinfall: ' + str(Parameters.rheinfall_options))
     rank_suffix = 'rank.txt'
     temp_rank_file = matrix_file + rank_suffix
-    print(temp_rank_file)
     rheinfall_command = "./rank-%s %s %s" % (rheinfall_option, matrix_file, temp_rank_file)
-    print(rheinfall_command)
     os.system(rheinfall_command)
     rank = int(StoreLoad.load_line(temp_rank_file))
     StoreLoad.delete_file_and_empty_dir(temp_rank_file)
