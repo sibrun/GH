b19f1d0c0ff3b283425ad4eafa4ee1cfe2201740
Simon Brun <sb.simonbrun@gmail.com>
2018-05-28 15:22:30 +0200

    fix path to linbox an rheinfall executable

--- source/LinboxInterface.py
+++ source/LinboxInterface.py
@@ -5,10 +5,11 @@ import StoreLoad
 def rank(matrix_file, prime=None):
     rank_suffix = 'rank.txt'
     temp_rank_file = matrix_file + rank_suffix
+    linbox_path = os.path.join(os.path.curdir, "source", "rank")
     if prime is None:
-        linbox_command = "./rank %s %s" % (matrix_file, temp_rank_file)
+        linbox_command = "%s %s %s" % (linbox_path, matrix_file, temp_rank_file)
     else:
-        linbox_command = "./rank %s %s %d" % (matrix_file, temp_rank_file, prime)
+        linbox_command = "%s %s %s %d" % (linbox_path, matrix_file, temp_rank_file, prime)
     os.system(linbox_command)
     rank = int(StoreLoad.load_line(temp_rank_file))
     StoreLoad.delete_file_and_empty_dir(temp_rank_file)
--- source/RheinfallInterface.py
+++ source/RheinfallInterface.py
@@ -8,7 +8,8 @@ def rank(rheinfall_option, matrix_file):
         raise ValueError('Possible options for rheinfall: ' + str(Parameters.rheinfall_options))
     rank_suffix = 'rank.txt'
     temp_rank_file = matrix_file + rank_suffix
-    rheinfall_command = "./rank-%s %s %s" % (rheinfall_option, matrix_file, temp_rank_file)
+    rheinfall_path = os.path.join(os.path.curdir, "source", "rank")
+    rheinfall_command = "%s-%s %s %s" % (rheinfall_path, rheinfall_option, matrix_file, temp_rank_file)
     os.system(rheinfall_command)
     rank = int(StoreLoad.load_line(temp_rank_file))
     StoreLoad.delete_file_and_empty_dir(temp_rank_file)
