895348180c1ccd082bbf0b713a26090df864f1e0
Simon Brun <sb.simonbrun@gmail.com>
2018-05-29 17:38:24 +0200

    Documentation

--- source/Shared.py
+++ source/Shared.py
@@ -13,14 +13,16 @@ class Perm:
     def __init__(self, p):
         """Initialize the permutation.
 
-        :param p: list(non-negative int): Image of the permutation with consecutive indices starting at zero.
+        :param p: Image of the permutation with consecutive indices starting at zero.
+        :type p: list(int)
         """
         self.p = p
 
     def inverse(self):
         """Return the inverse permutation.
 
-        :return: list(non-negative int): Image of the inverse permutation.
+        :return: Image of the inverse permutation.
+        :rtype: list(int)
         """
         inverse = [0] * len(self.p)
         for i, p in enumerate(self.p):
@@ -29,21 +31,24 @@ class Perm:
         #return [j-1 for j in Permutation([j+1 for j in self.p]).inverse()]
 
     def signature(self):
-        """Returns the sign of the permutation.
+        """Return the sign of the permutation.
 
-        :return: int: Sign of the permutation.
+        :return: Sign of the permutation.
+        :rtype: int
         """
         return Permutation([j+1 for j in self.p]).signature()
 
     @classmethod
     def shifted(cls, p):
-        """Generates a permutation not necessarily starting at zero.
+        """Generate a permutation not necessarily starting at zero.
 
-        Caution: The inverse of the returned permutation does not correspond to the inverse of the permutation induced by
+        :Note: The inverse of the returned permutation does not correspond to the inverse of the permutation induced by
         p but rather to the corresponding inverse permutation of the shifted indices starting at zero.
 
-        :param p: list(non-negative int): Image of the permutation with consecutive indices.
-        :return: Perm: Permutation of consecutive indices starting at zero.
+        :param p: Image of the permutation with consecutive indices.
+        :type p: list(int)
+        :return: Permutation of consecutive indices starting at zero.
+        :rtype: Perm
         """
         pmin = min(p)
         return cls([j - pmin for j in p])
@@ -57,9 +62,10 @@ class OrderedDict(collections.OrderedDict):
         super(OrderedDict, self).__init__(*args)
 
     def __str__(self):
-        """Returns the string of the ordered dictionary.
+        """Return the string of the ordered dictionary.
 
-        :return: str: Ordered dictionary as string.
+        :return: Ordered dictionary as string.
+        :rtype: str
         """
         s = '('
         for (key, value) in self.items():
@@ -68,17 +74,19 @@ class OrderedDict(collections.OrderedDict):
         return s
 
     def get_value_tuple(self):
-        """Returns the values as tuple.
+        """Return the values as tuple.
 
-        :return: tuple: Values of the ordered dictionary as tuple.
+        :return: Values of the ordered dictionary as tuple.
+        :rtype: tuple
         """
         return tuple(self.values())
 
 
 def enumerate_edges(graph):
-    """Labels the edges of the graph lexicographically.
+    """Label the edges of the graph lexicographically.
 
-    :param graph: sage.Graph: Input graph.
+    :param graph: Input graph.
+    :type graph: Graph
     """
     for (j, e) in enumerate(graph.edges(labels=False)):
         a, b = e
@@ -86,34 +94,42 @@ def enumerate_edges(graph):
 
 
 def edge_perm_sign(graph):
-    """Returns the sign of the permutation induced by the order of edges of the graph.
+    """Return the sign of the permutation induced by the order of edges of the graph.
 
     Edge labels are supposed to start at zero.
 
-    :param graph: sage.Graph: Input graph.
-    :return: int: Sign of the permutation induced by the order of edges of the graph.
+    :param graph: Input graph.
+    :type graph: Graph
+    :return: Sign of the permutation induced by the order of edges of the graph.
+    :rtype: int
     """
     p = [j for (a, b, j) in graph.edges()]
     return Perm(p).signature()
 
 
 def shifted_edge_perm_sign(graph):
-    """Returns the sign of the permutation induced by the order of edges of the graph.
+    """Return the sign of the permutation induced by the order of edges of the graph.
 
     Edge labels don't have to start at zero.
 
-    :param graph: sage.Graph: Input graph.
-    :return: int: Sign of the permutation induced by the order of edges of the graph.
+    :param graph: Input graph.
+    :type graph: Graph
+    :return: Sign of the permutation induced by the order of edges of the graph.
+    :rtype: int
     """
     p = [j for (a, b, j) in graph.edges()]
     return Perm.shifted(p).signature()
 
 
 def permute_to_left(pair, vertex_range):
-    """Permutes u,v to the left of the range vertex_range and returns the induced permutation.
+    """Permute pair to the left of the range vertex_range and returns the induced permutation.
 
-    :param vertex_range: range: Range of vertices.
-    :return: list(non-negative int): Permutation which permutes u,v to the left of the range vertex_range.
+    :param pair: Pair of vertex indices to be permuted to the left.
+    :type pair: tuple(int, int)
+    :param vertex_range: Range of vertices.
+    :type vertex_range: range
+    :return: Permutation which permutes u,v to the left of the range vertex_range.
+    :rtype: list(int)
     """
     (u, v) = pair
     p = list(vertex_range)
@@ -131,10 +147,12 @@ def permute_to_left(pair, vertex_range):
 
 
 def matrix_norm(M):
-    """Returns the Frobenius norm of the matrix M.
+    """Return the Frobenius norm of the matrix M.
 
-    :param M: sage.Matrix: Input matrix.
-    :return: non-negative int: Frobenius norm of the matrix M.
+    :param M: Input matrix.
+    :type M: Matrix
+    :return: Frobenius norm of the matrix M.
+    :rtype: int
     """
     return sqrt(sum(map(power_2, M.list())))
 
