a3b129ad2b306440785826c19187411fc2833d26
Simon Brun <sb.simonbrun@gmail.com>
2018-05-30 15:00:06 +0200

    Documentation

--- source/LinboxInterface.py
+++ source/LinboxInterface.py
@@ -1,3 +1,5 @@
+"""Interface to the linbox library for rank computations"""
+
 import os
 import tempfile
 import Parameters
@@ -9,6 +11,22 @@ linbox_options = {"rational", "mod"}    # Linbox options for rank computation: '
 
 
 def rank(linbox_option, matrix_file, prime=Parameters.prime):
+    """Call the linbox library to compute the rank of a matrix.
+
+    :param linbox_option: Options for rank computation: 'rational' (exact rank over the rational numbers),
+                          'mod' (rank over a finite field, i.e. all calculations modulo a prime number).
+    :type linbox_option: str
+    :param matrix_file: Path to the matrix file. The matrix needs to be stored in the SMS format.
+    :type matrix_file: path
+    :param prime: Prime number for modulo rank calculations (Default: Parameters.prime)
+    :type prime: int
+    :return: Matrix rank calculated by the linbox library.
+    :rtype: int
+
+    .. seealso:: - http://www.linalg.org/
+                 - https://github.com/linbox-team/linbox/blob/master/examples/rank.C
+                 - http://ljk.imag.fr/membres/Jean-Guillaume.Dumas/simc.html
+    """
     if not (linbox_option in linbox_options):
         raise ValueError('Possible options for linbox: ' + str(linbox_options))
     linbox_path = os.path.join(os.path.curdir, "source", "rank")
--- source/RheinfallInterface.py
+++ source/RheinfallInterface.py
@@ -1,3 +1,4 @@
+"""Interface to the rheinfall library for rank computations"""
 import os
 import tempfile
 
@@ -6,7 +7,21 @@ rheinfall_options = {"int64", "mpq", "mpz"}  # Rheinfall options for rank comput
                                              # modulo 64 bit integer), 'mpz' (exact rank over Z), 'mpq' (exact rank over
                                              # the rational numbers).
 
+
 def rank(rheinfall_option, matrix_file):
+    """Call the rheinfall library to compute the rank of a matrix.
+
+    :param rheinfall_option: Options for rank computation: 'int64' (rank calculation modulo 64 bit integer),
+                             'mpz' (exact rank over Z), 'mpq' (exact rank over the rational numbers).
+    :type rheinfall_option: str
+    :param matrix_file: Path to the matrix file. The matrix needs to be stored in the SMS format.
+    :type matrix_file: path
+    :return: Matrix rank calculated by the rheinfall library.
+    :rtype: int
+
+    .. seealso:: - https://github.com/riccardomurri/rheinfall/blob/master/src.c%2B%2B/examples/rank.cpp
+                 - http://ljk.imag.fr/membres/Jean-Guillaume.Dumas/simc.html
+    """
     if not (rheinfall_option in rheinfall_options):
         raise ValueError('Possible options for rheinfall: ' + str(rheinfall_options))
     rheinfall_path = os.path.join(os.path.curdir, "source", "rank")
