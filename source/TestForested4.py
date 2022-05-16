c855c84940d29a5d3d3e6bdd3d2fa5bf02fb1045
thomas <t.willwacher@gmail.com>
2022-05-16 08:10:18 +0200

    ...

--- source/TestForested4.py
+++ source/TestForested4.py
@@ -3,15 +3,18 @@ import CHairyGraphComplex
 from sage.all import *
 
 # vs = ForestedGraphComplex.PreForestedGVS(12,7,1,0)
-vs = ForestedGraphComplex.ForestedGVS(12,7,1,0, True)
-# for pvs in vs.get_required_prevs():
-#     pvs.build_basis()
-# vs.build_basis()
-print(vs.get_dimension())
-b1 = list(vs.get_basis())
+for m in range(5):
+    vs = ForestedGraphComplex.ForestedGVS(12,7,m,0, True)
+    # for pvs in vs.get_required_prevs():
+    #     pvs.build_basis()
+    # vs.build_basis()
+    print(m)
+    print(vs.get_dimension())
 
-b2 = [G for G in b1 if G.is_biconnected() ]
+    print( sum(1 for G in vs.get_basis() if G.is_biconnected() ) )
+    print( sum(1 for G in vs.get_basis() if len(list(G.bridges()))==0 ) )
+    # b2 = [G for G in b1 if G.is_biconnected() ]
 
-b3 = [G for G in b1 if len(list(G.bridges()))==0 ]
+    # b3 = [G for G in b1 if len(list(G.bridges()))==0 ]
 
-print(len(b1), len(b2), len(b3))
+    # print(len(b2), len(b3))
