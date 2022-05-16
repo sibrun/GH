b9d591c9b692e03f6bc081994aba109dbad91bae
thomas <t.willwacher@gmail.com>
2022-05-16 07:55:17 +0200

    ...

--- source/TestForested4.py
+++ source/TestForested4.py
@@ -7,10 +7,10 @@ vs = ForestedGraphComplex.ForestedGVS(12,7,0,0, True)
 #     pvs.build_basis()
 # vs.build_basis()
 print(vs.get_dimension())
-b1 = vs.get_basis()
+b1 = list(vs.get_basis())
 
 b2 = [G for G in b1 if G.is_biconnected() ]
 
 b3 = [G for G in b1 if len(list(G.bridges()))>0]
 
-print(len(list(b1)), len(b2), len(b3))
+print(len(b1), len(b2), len(b3))
