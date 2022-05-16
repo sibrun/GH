9dea959c85e2671efc02447e3c309c6151ede9d4
thomas <t.willwacher@gmail.com>
2022-05-16 07:53:12 +0200

    ...

--- source/TestForested4.py
+++ source/TestForested4.py
@@ -2,7 +2,11 @@ import ForestedGraphComplex
 import CHairyGraphComplex
 from sage.all import *
 
-vs = ForestedGraphComplex.ForestedGVS(12,7,0,0)
+vs = ForestedGraphComplex.ForestedGVS(12,7,0,0, True)
+# for pvs in vs.get_required_prevs():
+#     pvs.build_basis()
+# vs.build_basis()
+print(vs.get_dimension())
 b1 = vs.get_basis()
 
 b2 = [G for G in b1 if G.is_biconnected() ]
