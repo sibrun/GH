a8a519975d73034eb2d4d1f4589c75e4d4818043
thomas <t.willwacher@gmail.com>
2022-05-24 08:12:26 +0200

    ...

--- source/TestForested4.py
+++ source/TestForested4.py
@@ -2,7 +2,7 @@ import ForestedGraphComplex
 import CHairyGraphComplex
 from sage.all import *
 
-import timeit
+from timeit import default_timer as timer
 
 # vs = ForestedGraphComplex.PreForestedGVS(12,7,1,0)
 for m in range(12):
@@ -13,14 +13,14 @@ for m in range(12):
     print(m)
     print(vs.get_dimension())
 
-    start = timeit.timeit()
+    start = timer()
     print( sum(1 for G in vs.get_basis() if G.is_biconnected() ) )
-    end = timeit.timeit()
+    end = timer()
     print(end - start)
 
-    start = timeit.timeit()
+    start = timer()
     print( sum(1 for G in vs.get_basis() if len(list(G.bridges()))==0 ) )
-    end = timeit.timeit()
+    end = timer()
     print(end - start)
 
     # b2 = [G for G in b1 if G.is_biconnected() ]
