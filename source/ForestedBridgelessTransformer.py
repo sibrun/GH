96285dfd0379e6fb9811ea42fee448d31147c2fb
thomas <t.willwacher@gmail.com>
2022-05-24 12:51:47 +0200

    ...

--- source/ForestedBridgelessTransformer.py
+++ source/ForestedBridgelessTransformer.py
@@ -3,10 +3,12 @@ import StoreLoad
 import os
 import GraphOperator
 
-maskfile_ending = ".blmsk" # the masks are stored in files with this ending
+maskfile_ending = ".blmsk"  # the masks are stored in files with this ending
 
 if ForestedGraphComplex.use_bridgeless:
-    raise RuntimeError("ForestedBridgelessTransformer: Use this module only if use_bridgeless is False!")
+    raise RuntimeError(
+        "ForestedBridgelessTransformer: Use this module only if use_bridgeless is False!")
+
 
 def get_blfilepath(path):
     return path.replace("/forested/", "/forestedbl/")
@@ -14,13 +16,15 @@ def get_blfilepath(path):
 
 """Contains routines for transcribing existing forested data files 
 (with bridges) into bridgeless form."""
+
+
 class BridgelessMask():
     def __init__(self, vs) -> None:
         """ vs must be a ForestedGraphComplex or PreForestedGVS.
         WITH BRIDGES (...that is use_bridgeless must be off)"""
-        self.vs = vs 
+        self.vs = vs
         if isinstance(vs, ForestedGraphComplex.PreForestedGVS):
-            self.prevs = vs 
+            self.prevs = vs
         else:
             self.prevs = vs.preVS
 
@@ -40,26 +44,26 @@ class BridgelessMask():
     def compute_mask(self):
         if self.vs.is_valid() and self.vs.exists_basis_file() and not self.exists_maskfile():
             msk = self._compute_mask()
-            print("Generating mask: ", str(self.vs) )
+            print("Generating mask: ", str(self.vs))
             strmask = [str(m) for m in msk]
             StoreLoad.store_string_list(strmask, self.maskfile())
 
     def create_filtered_basisfile(self):
-        if not ( self.vs.is_valid() and self.vs.exists_basis_file() ):
+        if not (self.vs.is_valid() and self.vs.exists_basis_file()):
             return
-        newbasisfile = get_blfilepath( self.vs.get_basis_file_path() )
+        newbasisfile = get_blfilepath(self.vs.get_basis_file_path())
         if os.path.exists(newbasisfile):
             return
-        
+
         if not self.exists_maskfile():
             self.compute_mask()
 
-        print("Filtering basis: ",  str(self.vs) )
-        
+        print("Filtering basis: ",  str(self.vs))
+
         mask = self.load_mask()
         basisg6 = self.vs.get_basis_g6()
 
-        filteredbasis = [s for (s,m) in zip(basisg6,mask) if m == 1]
+        filteredbasis = [s for (s, m) in zip(basisg6, mask) if m == 1]
         filteredbasis.insert(0, str(len(filteredbasis)))
         StoreLoad.store_string_list(filteredbasis, newbasisfile)
 
@@ -67,22 +71,23 @@ class BridgelessMask():
         """dict from all indices to bridgeless indices"""
         msk = self.load_mask()
         mski = enumerate(msk)
-        ifiltered = ( i for (i,m) in mski if m==1)
-        return { i:j for (j,i) in enumerate(ifiltered) }
+        ifiltered = (i for (i, m) in mski if m == 1)
+        return {i: j for (j, i) in enumerate(ifiltered)}
 
     def filtered_dim(self):
         return sum(self.load_mask())
 
+
 class BridgeLessMaskOM():
-    def __init__(self, op : GraphOperator.OperatorMatrix):
+    def __init__(self, op: GraphOperator.OperatorMatrix):
         self.op = op
         self.domain = BridgelessMask(op.domain)
         self.target = BridgelessMask(op.target)
 
     def create_filtered_matrixfile(self):
-        if not (self.op.is_valid() and self.op.exists_matrix_file() ):
+        if not (self.op.is_valid() and self.op.exists_matrix_file()):
             return
-        
+
         newfilepath = get_blfilepath(self.op.get_matrix_file_path())
         if os.path.exists(newfilepath):
             return
@@ -90,13 +95,13 @@ class BridgeLessMaskOM():
         print("Translating operator: ", str(self.op))
         idict1 = self.domain.index_dict()
         idict2 = self.target.index_dict()
-        
-        lst, oldshape = self.op._load_matrix_list()
-        newshape = (self.domain.filtered_dim(), self.target.filtered_dim()  )
 
-        matrix_list = [ (idict1[i],idict2[j], v) 
-                    for (i,j,v) in lst if ( i in idict1 and j in idict2) ]
-        
+        lst, oldshape = self.op._load_matrix_list()
+        newshape = (self.domain.filtered_dim(), self.target.filtered_dim())
+
+        matrix_list = [(idict1[i], idict2[j], v)
+                       for (i, j, v) in lst if (i in idict1 and j in idict2)]
+
         # copied
         data_type = "M"
         (d, t) = newshape
@@ -107,35 +112,32 @@ class BridgeLessMaskOM():
         stringList.append("0 0 0")
         StoreLoad.store_string_list(stringList, newfilepath)
 
-        
 
-h_range=range(0,2)
-v_range=range(0,15)
-l_range=range(0,6)
-m_range=range(0,15)
+h_range = range(0, 9)
+v_range = range(0, 25)
+l_range = range(0, 10)
+m_range = range(0, 25)
 for h in h_range:
     for l in l_range:
         for v in v_range:
             for m in m_range:
-                bm = BridgelessMask(ForestedGraphComplex.PreForestedGVS(v,l,m,h))
+                bm = BridgelessMask(
+                    ForestedGraphComplex.PreForestedGVS(v, l, m, h))
                 bm.create_filtered_basisfile()
                 for even_edges in [True, False]:
-                    bm = BridgelessMask(ForestedGraphComplex.ForestedGVS(v,l,m,h,even_edges))
+                    bm = BridgelessMask(
+                        ForestedGraphComplex.ForestedGVS(v, l, m, h, even_edges))
                     bm.create_filtered_basisfile()
-                   
+
 
 for h in h_range:
     for l in l_range:
         for v in v_range:
             for m in m_range:
                 for even_edges in [True, False]:
-                    bo = BridgeLessMaskOM(ForestedGraphComplex.ContractEdgesGO.generate_operator(v,l,m,h,even_edges))
+                    bo = BridgeLessMaskOM(
+                        ForestedGraphComplex.ContractEdgesGO.generate_operator(v, l, m, h, even_edges))
                     bo.create_filtered_matrixfile()
-                    bo = BridgeLessMaskOM(ForestedGraphComplex.UnmarkEdgesGO.generate_operator(v,l,m,h,even_edges))
-                    bo.create_filtered_matrixfile()                    
-           
-    
-    
-
-        
-        
+                    bo = BridgeLessMaskOM(
+                        ForestedGraphComplex.UnmarkEdgesGO.generate_operator(v, l, m, h, even_edges))
+                    bo.create_filtered_matrixfile()
