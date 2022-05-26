aa1457de71f19e273654e4af5e5d17a1001b38f7
thomas <t.willwacher@gmail.com>
2022-05-26 19:56:06 +0200

    ...

--- source/GCDimensions.py
+++ source/GCDimensions.py
@@ -42,7 +42,7 @@ sGCdims = \
 0 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24"""
 
 # edges, vertices
-GCdims = [ s.split(" ")[1:] for s in sGCdims.splitlines()]
+GCdims = [ [int(w) for w in s.split(" ")[1:] ] for s in sGCdims.splitlines()]
 max_vertices = 24
 max_edges = 36
 
