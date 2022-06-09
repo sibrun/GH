f48fa0e4f90ecf27510eb1e8998c7e171993ca02
thomas <t.willwacher@gmail.com>
2022-06-09 17:10:31 +0200

    ...

--- source/Test6.py
+++ source/Test6.py
@@ -9,6 +9,7 @@ import StoreLoad
 import OrdinaryMerkulovComplex
 import GraphOperator
 import ForestedGraphComplex
+import os
 
 # even_e = True
 # OGC = OrdinaryMerkulovComplex.OrdinaryMerkulovGC(range(20), range(9), even_e, ['contract'])
@@ -50,6 +51,52 @@ def precond_stats(op:GraphOperator.OperatorMatrix):
     print(f"Two rows:  {tworows}")
     print(f"Two cols:  {twocols}")
     print(f"multis:    {multis}")
+
+
+def get_submatrix(lst, keeprow, keepcol):
+    print("Submatrix...")
+    newm = sum(1 for b in keeprow if b)
+    newn = sum(1 for b in keepcol if b)
+    
+    newrowinds = [i for (i,b) in enumerate(keeprow) if b]
+    newcolinds = [j for (j,b) in enumerate(keepcol) if b]
+    
+    # dict from old index to new
+    rowdict = { iold : inew for (inew, iold) in enumerate(newrowinds) }
+    coldict = { iold : inew for (inew, iold) in enumerate(newcolinds) }
+
+    newlst = [ (rowdict[i],coldict[j],v) for (i,j,v) in lst 
+                                if keeprow[i] and keepcol[j] ]
+
+    return (newlst, newm, newn)
+
+def graphcheck(lst, m, n):
+    rcount = [0 for _ in range(m)]
+    ccount = [0 for _ in range(n)]
+    for (i,j, v) in lst:
+        rcount[i] += 1
+        ccount[j] += 1
+    
+    # consider only columns with 2 nonzero entries
+    cols = [c == 2 for c in ccount]
+    lst2, m2, n2 =  get_submatrix(lst, [True for _ in range(m)], cols)
+    print("Generating graph")
+    # make graph
+    G = Graph(m2)
+    edges = []
+    es = [-1 for _ in range(n2)] 
+    for (i,j,v) in lst2:
+        if es[j] >0:
+            edges.append[(i, es[j])]
+        else:
+            es[j] = i
+    G.add_edges(edges)
+    print("Graph generated, finding ccs")
+
+    ccs = G.connected_components()
+    ccsizes = [len(a) for a in ccs]
+    print(ccsizes[0:100])
+
 
 
 def removerstep(lst, m,n, rankbias):
@@ -111,6 +158,30 @@ def removerstep(lst, m,n, rankbias):
 
     return (newlst, newm, newn, newrankbias)
 
+def load_sms_file(fname):
+    if not os.path.isfile(fname):
+            raise StoreLoad.FileNotFoundError(
+                "Cannot load matrix, No matrix file found for %s: " % str(self))
+    stringList = StoreLoad.load_string_list(self.get_matrix_file_path())
+    (d, t, data_type) = stringList.pop(0).split(" ")
+    shape = (d, t) = (int(d), int(t))
+    
+    tail = map(int, stringList.pop().split(" "))
+    if not list(tail) == [0, 0, 0]:
+        raise ValueError("%s: End line missing or matrix not correctly read from file"
+                            % fname)
+    matrix_list = []
+    for line in stringList:
+        (i, j, v) = map(int, line.split(" "))
+        if i < 1 or j < 1:
+            raise ValueError("%s: Invalid matrix index: %d %d" %
+                                (fname, i, j))
+        if i > d or j > t:
+            raise ValueError("%s: Invalid matrix index outside matrix size:"
+                                " %d %d" % (fname, i, j))
+        matrix_list.append((i - 1, j - 1, v))
+    return (matrix_list, shape)
+
 def precondition(op:GraphOperator.OperatorMatrix):
     print("Loading...: ", str(op))
     (lst,(m,n)) = op._load_matrix_list()
@@ -139,5 +210,11 @@ def precondition(op:GraphOperator.OperatorMatrix):
 # precond_stats(ForestedGraphComplex.ContractUnmarkTopBiOM.generate_operator(7,8,0, True))
 # precond_stats(ForestedGraphComplex.ContractUnmarkTopBiOM.generate_operator(7,10,0, False))
 
-precondition( ForestedGraphComplex.ContractUnmarkTopBiOM.generate_operator(7,9,0, False) )
+# precondition( ForestedGraphComplex.ContractUnmarkTopBiOM.generate_operator(7,9,0, False) )
 # precondition( OrdinaryGraphComplex.ContractEdgesGO.generate_operator(12,10, True) )
+
+fff = "gh_data/data/forestedbl/odd_edges/bi_D_contract_unmark_top_7_9_0.txt.preconditioned_978418.txt"
+
+lst, (m,n) = load_sms_file(fff)
+
+graphcheck(lst, m, n)
