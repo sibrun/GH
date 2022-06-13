401bb744708af3f86d7070963b02155e0b413410
thomas <t.willwacher@gmail.com>
2022-06-14 00:14:33 +0200

    ...

--- source/TestHairyMerkulov.py
+++ source/TestHairyMerkulov.py
@@ -1,6 +1,6 @@
 import HairyMerkulovComplex
 
-HGC = HairyMerkulovComplex.HairyMerkulovGC(range(15), range(5), range(1,3), True, True)
+HGC = HairyMerkulovComplex.HairyMerkulovGC(range(15), range(5), range(1,3), True, True, differentials=['contract'])
 HGC.build_basis()
 HGC.build_matrix()
 HGC.compute_rank(sage="integer")
