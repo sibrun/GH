7ac4e53555551d89025ef7231747970977bf21f9
thomas <t.willwacher@gmail.com>
2022-07-06 17:45:07 +0200

    ...

---
+++ source/CHairyBuildAll.py
@@ -0,0 +1,29 @@
+import CHairyGraphComplex
+import GraphVectorSpace
+
+"""Builds bases of all hairy vector spaces in the "computable" range.
+"""
+
+
+if __name__ == "__main__":
+    nr_jobs = 10
+    print(f"Building all computable hairy bases using {nr_jobs} jobs ...")
+    vs_list = []
+
+    for even_e in [True, False]:
+        vs_list = vs_list + [CHairyGraphComplex.CHairyGraphVS(v, l, 1, even_e)
+                             for v in range(18) for l in range(8)]
+        vs_list = vs_list + [CHairyGraphComplex.CHairyGraphVS(v, l, 2, even_e)
+                             for v in range(18) for l in range(8)]
+        vs_list = vs_list + [CHairyGraphComplex.CHairyGraphVS(v, l, 3, even_e)
+                             for v in range(18) for l in range(8)]
+        vs_list = vs_list + [CHairyGraphComplex.CHairyGraphVS(v, l, 4, even_e)
+                             for v in range(18) for l in range(7)]
+        vs_list = vs_list + [CHairyGraphComplex.CHairyGraphVS(v, l, 5, even_e)
+                             for v in range(18) for l in range(6)]
+
+    sumvs = GraphVectorSpace.SumVectorSpace(vs_list)
+
+    sumvs.build_basis(n_jobs=nr_jobs)
+
+    print("Finished computing chairy bases.")
