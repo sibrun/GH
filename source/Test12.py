8a06046cf470fd091afcb7b4ff11c71b0af4e211
thomas <t.willwacher@gmail.com>
2022-09-13 16:51:56 +0200

    ...

--- source/Test12.py
+++ source/Test12.py
@@ -4,11 +4,12 @@ import Parallel
 import BufferedGeng
 
 
-if __name__ == "__main__":
+def fill_cache(ve):
+    v, e = ve
+    BufferedGeng.fill_cache(v, e, False)
 
-    def fill_cache(ve):
-        v, e = ve
-        BufferedGeng.fill_cache(v, e, False)
+
+if __name__ == "__main__":
 
     ve_pairs = [(v, l+v-1) for l in range(11, 12) for v in range(2*l-1)]
     Parallel.parallel(fill_cache, ve_pairs, n_jobs=10)
