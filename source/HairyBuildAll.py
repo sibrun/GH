7b49104cd3a24fe1197121233a6d89ee944b4ad8
thomas <t.willwacher@gmail.com>
2022-09-13 16:25:47 +0200

    ...

--- source/HairyBuildAll.py
+++ source/HairyBuildAll.py
@@ -2,8 +2,11 @@ import HairyGraphComplex
 import GraphVectorSpace
 
 """Builds bases of all hairy vector spaces in the "computable" range.
+
+    computable means complexity <=max_complexity.
 """
 
+max_complexity = 10
 
 if __name__ == "__main__":
     nr_jobs = 10
@@ -12,19 +15,25 @@ if __name__ == "__main__":
 
     for even_e in [True, False]:
         for even_h in [True, False]:
-            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v,l,1,even_e,even_h)
-                                for v in range(18) for l in range(10)]
-            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v,l,2,even_e,even_h)
-                                for v in range(18) for l in range(9)]
-            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v,l,3,even_e,even_h)
-                                for v in range(18) for l in range(7)]
-            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v,l,4,even_e,even_h)
-                                for v in range(18) for l in range(6)]
-            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v,l,5,even_e,even_h)
-                                for v in range(18) for l in range(6)]    
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v,l,1,even_e,even_h)
+            #                     for v in range(18) for l in range(10)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 2, even_e, even_h)
+                                 for v in range(20) for l in range(max_complexity-1)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 3, even_e, even_h)
+                                 for v in range(20) for l in range(max_complexity-1)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 4, even_e, even_h)
+                                 for v in range(20) for l in range(max_complexity-2)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 5, even_e, even_h)
+                                 for v in range(20) for l in range(max_complexity-3)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 6, even_e, even_h)
+                                 for v in range(20) for l in range(max_complexity-4)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 7, even_e, even_h)
+                                 for v in range(20) for l in range(max_complexity-5)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 8, even_e, even_h)
+                                 for v in range(20) for l in range(max_complexity-6)]
 
     sumvs = GraphVectorSpace.SumVectorSpace(vs_list)
 
-    sumvs.build_basis(n_jobs = nr_jobs)
+    sumvs.build_basis(n_jobs=nr_jobs)
 
     print("Finished computing hairy bases.")
