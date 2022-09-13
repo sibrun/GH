42b83af27fc6b61730686279a5c3be56e15a2312
thomas <t.willwacher@gmail.com>
2022-09-13 14:21:07 +0200

    ...

--- source/Test10.py
+++ source/Test10.py
@@ -1,6 +1,6 @@
 import HairyGraphComplex
 
-l = 6
+l = 4
 v = 2*l-2
-V = HairyGraphComplex.HairyGraphVS(v, l, 1, True, True)
+V = HairyGraphComplex.HairyGraphVS(v, l, 2, True, True)
 V.test_graph_generation()
