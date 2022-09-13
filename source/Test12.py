3ae4854923a1dc06938a79b61d276d34a4414d34
thomas <t.willwacher@gmail.com>
2022-09-13 16:49:07 +0200

    ...

---
+++ source/Test12.py
@@ -0,0 +1,14 @@
+# fill nauty cache
+
+import Parallel
+import BufferedGeng
+
+
+if __name__ == "__main__":
+
+    def fill_cache(ve):
+        v, e = ve
+        BufferedGeng.fill_cache(v, e, False)
+
+    ve_pairs = [(v, l+v-1) for l in range(11, 12) for v in range(2*l-1)]
+    Parallel.parallel(fill_cache, ve_pairs, n_jobs=10)
