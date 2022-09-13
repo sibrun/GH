a93b5717867e6c95b8eac2f58710b59672339b77
thomas <t.willwacher@gmail.com>
2022-09-13 15:20:24 +0200

    ...

--- source/HairyGraphComplex.py
+++ source/HairyGraphComplex.py
@@ -293,6 +293,7 @@ class HairyGraphVS(GraphVectorSpace.GraphVectorSpace):
             othervs = HairyGraphVS(
                 self.n_vertices-1, self.n_loops-1, 2, self.even_edges, True)
             nv = self.n_vertices
+            print(othervs.exists_basis_file())
             for G in othervs.get_basis():
                 GG = copy(G)
                 # print("orig2: ", othervs.graph_to_canon_g6(G)[0])
