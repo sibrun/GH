9b8b17eb6fee74eb39829be988a04ee41bcf6649
thomas <t.willwacher@gmail.com>
2022-09-13 17:00:09 +0200

    ...

--- source/Test12.py
+++ source/Test12.py
@@ -11,5 +11,5 @@ def fill_cache(ve):
 
 if __name__ == "__main__":
 
-    ve_pairs = [(v, l+v-1) for l in range(11, 12) for v in range(2*l-1)]
+    ve_pairs = [(v, l+v-1) for l in range(0, 10) for v in range(2*l-1)]
     Parallel.parallel(fill_cache, ve_pairs, n_jobs=10)
