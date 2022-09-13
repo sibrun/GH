1735a83f16546f85dab9f84b65532910adc7be69
thomas <t.willwacher@gmail.com>
2022-09-13 16:00:03 +0200

    ...

--- source/Test11.py
+++ source/Test11.py
@@ -10,15 +10,18 @@ def get_g6_files(path):
             yield file
 
 
-path1 = "gh_data/data/hairy/even_edges_odd_hairs"
-path2 = "gh_data/data/hairy3/even_edges_odd_hairs"
-
-for file in get_g6_files(path2):
-    fullfile1 = os.path.join(path1, file)
-    fullfile2 = os.path.join(path2, file)
-    if os.path.exists(fullfile1):
-        with open(fullfile1) as f:
-            txt1 = f.read()
-        with open(fullfile2) as f:
-            txt2 = f.read()
-        print(f"Comparing {file}: {'OK' if txt1 == txt2 else 'ERROR'}")
+for path1, path2 in [("gh_data/data/hairy/even_edges_odd_hairs", "gh_data/data/hairy3/even_edges_odd_hairs"),
+                     ("gh_data/data/hairy/even_edges_even_hairs",
+                         "gh_data/data/hairy3/even_edges_even_hairs"),
+                     ("gh_data/data/hairy/odd_edges_odd_hairs",
+                         "gh_data/data/hairy3/odd_edges_odd_hairs"),
+                     ("gh_data/data/hairy/odd_edges_even_hairs", "gh_data/data/hairy3/odd_edges_even_hairs")]:
+    for file in get_g6_files(path2):
+        fullfile1 = os.path.join(path1, file)
+        fullfile2 = os.path.join(path2, file)
+        if os.path.exists(fullfile1):
+            with open(fullfile1) as f:
+                txt1 = f.read()
+            with open(fullfile2) as f:
+                txt2 = f.read()
+            print(f"Comparing {file}: {'OK' if txt1 == txt2 else 'ERROR'}")
