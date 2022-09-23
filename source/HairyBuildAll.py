2da92c11aad6e0f1ee31eb4c24b1f09c5ceac9f7
thomas <t.willwacher@gmail.com>
2022-09-23 09:59:09 +0200

    ...

--- source/HairyBuildAll.py
+++ source/HairyBuildAll.py
@@ -9,14 +9,14 @@ import GraphVectorSpace
 max_complexity = 11
 
 if __name__ == "__main__":
-    nr_jobs = 20
+    nr_jobs = 5
     print(f"Building all computable hairy bases using {nr_jobs} jobs ...")
     vs_list = []
 
     for even_e in [True, False]:
         for even_h in [True, False]:
-            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v,l,1,even_e,even_h)
-                                for v in range(18) for l in range(max_complexity-1)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 1, even_e, even_h)
+                                 for v in range(18) for l in range(max_complexity-1)]
             # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 2, even_e, even_h)
             #                      for v in range(20) for l in range(max_complexity-1)]
             # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 3, even_e, even_h)
