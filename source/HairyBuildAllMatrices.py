45cb068c4c2b787fe60cd9279674c9ef2f18055b
thomas <t.willwacher@gmail.com>
2022-09-26 14:13:03 +0200

    ...

--- source/HairyBuildAllMatrices.py
+++ source/HairyBuildAllMatrices.py
@@ -7,7 +7,7 @@ import GraphOperator
 
 
 if __name__ == "__main__":
-    nr_jobs = 10
+    nr_jobs = 20
     print(f"Building all computable hairy matrices using {nr_jobs} jobs ...")
     vs_list = []
     op_list = []
@@ -18,36 +18,36 @@ if __name__ == "__main__":
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 1, even_e, even_h)
                                  for v in range(18) for l in range(10)]
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 2, even_e, even_h)
-                                 for v in range(18) for l in range(9)]
+                                 for v in range(18) for l in range(10)]
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 3, even_e, even_h)
-                                 for v in range(18) for l in range(8)]
+                                 for v in range(18) for l in range(9)]
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 4, even_e, even_h)
-                                 for v in range(18) for l in range(7)]
+                                 for v in range(18) for l in range(9)]
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 5, even_e, even_h)
-                                 for v in range(18) for l in range(6)]
+                                 for v in range(18) for l in range(7)]
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 6, even_e, even_h)
-                                 for v in range(18) for l in range(5)]
+                                 for v in range(18) for l in range(6)]
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 7, even_e, even_h)
-                                 for v in range(18) for l in range(4)]
+                                 for v in range(18) for l in range(5)]
             vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 8, even_e, even_h)
-                                 for v in range(18) for l in range(4)]
+                                 for v in range(18) for l in range(5)]
 
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 1, even_e, even_h)
                                  for v in range(18) for l in range(10)]
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 2, even_e, even_h)
-                                 for v in range(18) for l in range(9)]
+                                 for v in range(18) for l in range(10)]
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 3, even_e, even_h)
-                                 for v in range(18) for l in range(8)]
+                                 for v in range(18) for l in range(9)]
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 4, even_e, even_h)
-                                 for v in range(18) for l in range(7)]
+                                 for v in range(18) for l in range(8)]
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 5, even_e, even_h)
-                                 for v in range(18) for l in range(6)]
+                                 for v in range(18) for l in range(7)]
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 6, even_e, even_h)
-                                 for v in range(18) for l in range(5)]
+                                 for v in range(18) for l in range(6)]
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 7, even_e, even_h)
-                                 for v in range(18) for l in range(4)]
+                                 for v in range(18) for l in range(5)]
             op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 8, even_e, even_h)
-                                 for v in range(18) for l in range(4)]
+                                 for v in range(18) for l in range(5)]
 
     sumvs = GraphVectorSpace.SumVectorSpace(vs_list)
 
