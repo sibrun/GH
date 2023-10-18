9ad3b7d2f4049a924252c84d3860107e559d3aeb
thomas <t.willwacher@gmail.com>
2023-10-18 17:10:36 +0200

    ...

---
+++ source/TestBVCyclic4.py
@@ -0,0 +1,24 @@
+import BVCyclic
+import OrdinaryGraphComplex
+import GraphVectorSpace
+import GraphOperator
+
+nr_jobs = 10
+print(f"Building GOneVS bases using {nr_jobs} jobs ...")
+sumvs = GraphVectorSpace.SumVectorSpace( [ BVCyclic.GOneVS(v, l) for v in range(0,20) for l in range(0,9) ] )
+
+sumvs.build_basis(n_jobs=nr_jobs)
+
+print("Finished computing bases.")
+
+op_list = [ BVCyclic.ReconnectEdgesGO.generate_operator(v, l) for v in range(0,20) for l in range(0,9) ]
+
+allop = GraphOperator.OperatorMatrixCollection(sumvs, op_list)
+
+allop.build_matrix(n_jobs=nr_jobs)
+
+print("Finished computing matrices.")
+
+print("computing ranks")
+allop.compute_rank(linbox="rational", n_jobs=nr_jobs)
+print("Finished")
