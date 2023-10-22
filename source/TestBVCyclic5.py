02bc9b5cc63135830ab6ef7cd9b7ec9a9d721ef0
thomas <t.willwacher@gmail.com>
2023-10-22 14:10:15 +0200

    ...

--- source/TestBVCyclic5.py
+++ source/TestBVCyclic5.py
@@ -1,12 +1,19 @@
 import BVCyclic
 
 
+
+
+
+
+
 ovs1 = BVCyclic.GOneVS(9,6)
 ovs2 = BVCyclic.GOneVS(8,6)
 
 l = list(ovs1.get_generating_graphs2())
 print(len(l))
-print(ovs1.graph_to_canon_g6( l[25]))
+G = l[500]
+autom_list = G.automorphism_group().gens()
+print(G.graph6_string(), ovs1._has_odd_automorphisms(G,autom_list))
 ovs1.build_basis(ignore_existing_files=True)
 print(ovs1.get_dimension())
 
