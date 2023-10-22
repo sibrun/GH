121eb9da39719c6409752a075de861cea443fce9
thomas <t.willwacher@gmail.com>
2023-10-22 13:56:46 +0200

    ...

--- source/TestBVCyclic5.py
+++ source/TestBVCyclic5.py
@@ -5,7 +5,9 @@ ovs1 = BVCyclic.GOneVS(9,6)
 ovs2 = BVCyclic.GOneVS(8,6)
 
 l = ovs1.get_generating_graphs2()
-print(list(l))
+print(len(list(l)))
+ovs1.build_basis()
+print(ovs1.get_dimension())
 
 # print(vs.get_dimension(), op1.get_matrix_rank(), op2.get_matrix_rank(), opc.get_matrix_rank())
 # print(ovs1.get_dimension(), ovs2.get_dimension())
