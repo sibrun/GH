3acdd7d849b7fbc7eb1f04940c40614cd6d334d3
thomas <t.willwacher@gmail.com>
2023-10-28 16:51:45 +0200

    ...

---
+++ source/BVCyclicGenerateAll2.py
@@ -0,0 +1,36 @@
+
+import BVCyclic
+import OrdinaryGraphComplex
+import GraphVectorSpace
+import GraphOperator
+
+
+if __name__ == "__main__":
+    nr_jobs = 20
+    max_loops = 8
+    max_vert = 16
+    ignore_ex = False
+
+    # print(f"Building GOneVS bases using {nr_jobs} jobs ...")
+    sumvs = GraphVectorSpace.SumVectorSpace( [ OrdinaryGraphComplex.OrdinaryGVS(v, l) for v in range(0,max_vert+1) for l in range(0,max_loops+1) ] )
+
+    # sumvs.build_basis(n_jobs=nr_jobs, ignore_existing_files=ignore_ex)
+
+    # print("Finished computing bases.")
+
+    # op_list = [ BVCyclic.ReconnectEdgesGO.generate_operator(v, l) for v in range(0,20) for l in range(0,9) ]
+    # op_list = [ BVCyclic.ContractReconnectBiOM.generate_operator(v, l) for v in range(0,20) for l in range(0,8) ]
+
+    # allop = GraphOperator.OperatorMatrixCollection(sumvs, op_list)
+
+    op_list = [ BVCyclic.ContractAReconnectDeleteTriOM.generate_operator(v,l) for v in range(0,max_vert+1) for l in range(0,max_loops+1) ]
+    op_list += [ BVCyclic.AReconnectDeleteBiOM .generate_operator(v,l) for v in range(0,max_vert+1) for l in range(0,max_loops+1) ]
+    allop = GraphOperator.OperatorMatrixCollection(sumvs, op_list)
+    allop.build_matrix(n_jobs=nr_jobs, ignore_existing_files=ignore_ex)
+    
+    print("Finished computing matrices.")
+    print("computing ranks")
+    allop.compute_rank(sage="integer", n_jobs=nr_jobs, ignore_existing_files=ignore_ex)
+    # allop.compute_rank(linbox="rational", n_jobs=nr_jobs, ignore_existing_files=ignore_ex)
+    
+    print("Finished")
---
+++ source/TestBVCyclicCohom5.py
@@ -0,0 +1,79 @@
+import BVCyclic
+import OrdinaryGraphComplex
+
+import GenerateLatexTables
+
+def cohom_formatted_forested_top(D1, D2, Dc2, use_Dc2_rank=None, iso_dict=None, use_D1domain = True):
+    vs = D1.get_domain() if use_D1domain else D2.get_target()
+    if not vs.is_valid():
+        return "-"
+    if not vs.exists_basis_file():
+        return "?v"
+    d = vs.get_dimension()
+
+    r1 = 0
+    r2 = 0
+    rc2 = 0
+
+    if Dc2.is_valid():
+        if use_Dc2_rank is not None:
+            if use_Dc2_rank == "?":
+                return "?d"
+            else:
+                rc2 = int(use_Dc2_rank)
+        elif Dc2.exists_rank_file():
+            rc2 = Dc2.get_matrix_rank()
+        else:
+            return "?c2"
+
+    if D1.is_valid():
+        if D1.exists_rank_file():
+            r1 = D1.get_matrix_rank()
+        else:
+            return "?1"
+    if D2.is_valid():
+        if D2.exists_rank_file():
+            r2 = D2.get_matrix_rank()
+        else:
+            return "?2"
+
+    # exact or not?
+    r_str = "" 
+
+    # iso string
+    cohomdim = d+rc2-r1-r2
+    iso_str = ""
+
+    return str(cohomdim) + r_str + iso_str
+
+
+def create_ordinarycyclic_cohom_table(v_range, l_range):
+    s = ""
+
+    header =  [str(v) for v in v_range]
+    print(header)
+    for even_edges in [False]:
+        data = []
+        for l in l_range:
+            print(
+                str(l),
+                [ cohom_formatted_forested_top(
+                    BVCyclic.ContractAReconnectDeleteTriOM.generate_operator(v, l),
+                    BVCyclic.ContractAReconnectDeleteTriOM.generate_operator(v+1, l),
+                    BVCyclic.AReconnectDeleteBiOM.generate_operator(v, l),
+                    use_D1domain=False
+                )  for v in v_range])
+    return s
+
+create_ordinarycyclic_cohom_table(range(4, 22), range(3, 12))
+
+
+# op1 = BVCyclic.ContractReconnectBiOM.generate_operator(10,6)
+# op2 = BVCyclic.ContractReconnectBiOM.generate_operator(11,6)
+# vs = op2.get_target()
+# opc = BVCyclic.ReconnectEdgesGO.generate_operator(8,6)
+# ovs1 = BVCyclic.GOneVS(9,6)
+# ovs2 = BVCyclic.GOneVS(8,6)
+
+# print(vs.get_dimension(), op1.get_matrix_rank(), op2.get_matrix_rank(), opc.get_matrix_rank())
+# print(ovs1.get_dimension(), ovs2.get_dimension())
