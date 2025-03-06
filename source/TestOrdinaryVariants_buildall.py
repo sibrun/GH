a133723dbad0051426a1e8e8118039bfec0926c0
wilthoma <t.willwacher@gmail.com>
2025-03-06 21:59:45 +0100

    ...

---
+++ source/TestOrdinaryVariants_buildall.py
@@ -0,0 +1,63 @@
+import HairyGraphComplex
+import GraphVectorSpace
+import GraphOperator
+
+"""Builds bases of all hairy vector spaces in the "computable" range.
+"""
+
+
+if __name__ == "__main__":
+    nr_jobs = 15
+    print(f"Building all computable hairy matrices using {nr_jobs} jobs ...")
+    vs_list = []
+    op_list = []
+
+    for even_e in [True, False]:
+        for even_h in [True, False]:
+
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 1, even_e, even_h)
+            #                      for v in range(18) for l in range(10)]
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 2, even_e, even_h)
+            #                      for v in range(18) for l in range(9)]
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 3, even_e, even_h)
+            #                      for v in range(18) for l in range(8)]
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 4, even_e, even_h)
+            #                      for v in range(18) for l in range(7)]
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 5, even_e, even_h)
+            #                      for v in range(18) for l in range(7)]
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 6, even_e, even_h)
+            #                      for v in range(18) for l in range(6)]
+            # vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 7, even_e, even_h)
+            #                      for v in range(18) for l in range(6)]
+            vs_list = vs_list + [HairyGraphComplex.HairyGraphVS(v, l, 8, even_e, even_h)
+                                 for v in range(18) for l in range(6)]
+
+            # op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 1, even_e, even_h)
+            #                      for v in range(18) for l in range(10)]
+            # op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 2, even_e, even_h)
+            #                      for v in range(18) for l in range(9)]
+            # op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 3, even_e, even_h)
+            #                      for v in range(18) for l in range(8)]
+            # op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 4, even_e, even_h)
+            #                      for v in range(18) for l in range(7)]
+            # op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 5, even_e, even_h)
+            #                      for v in range(18) for l in range(7)]
+            # op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 6, even_e, even_h)
+            #                      for v in range(18) for l in range(6)]
+            # op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 7, even_e, even_h)
+            #                      for v in range(18) for l in range(6)]
+            op_list = op_list + [HairyGraphComplex.ContractEdgesGO.generate_operator(v, l, 8, even_e, even_h)
+                                 for v in range(18) for l in range(6)]
+
+    sumvs = GraphVectorSpace.SumVectorSpace(vs_list)
+
+    allop = GraphOperator.OperatorMatrixCollection(sumvs, op_list)
+
+    # sumvs.build_basis(n_jobs=nr_jobs)
+    allop.build_matrix(n_jobs=nr_jobs)
+
+    print("Finished computing hairy matrices.")
+
+    print("computing ranks")
+    allop.compute_rank(linbox="mod", n_jobs=nr_jobs)
+    print("Finished")
---
+++ source/TestOrdinaryVariants_buildallmatrices.py
@@ -0,0 +1,52 @@
+import OrdinaryVariants
+import GraphVectorSpace
+import GraphOperator
+
+"""Builds bases of all hairy vector spaces in the "computable" range.
+"""
+
+
+if __name__ == "__main__":
+    nr_jobs = 15
+    print(f"Building all computable hairy matrices using {nr_jobs} jobs ...")
+    vs_listf = []
+    vs_lista = []
+    op_list = []
+    maxl = 10
+
+    for even_e in [True, False]:
+
+        vs_listf = vs_listf + [OrdinaryVariants.OrdinaryGVSFull(v, l, even_e)
+                                for v in range(18) for l in range(maxl+1)]
+        vs_lista = vs_lista + [OrdinaryVariants.OrdinaryGVSBridgeless(v, l, even_e)
+                                for v in range(18) for l in range(maxl+1)]
+        vs_lista = vs_lista + [OrdinaryVariants.OrdinaryGVSTriconnected(v, l, even_e)
+                                for v in range(18) for l in range(maxl+1)]
+
+        op_list = op_list + [OrdinaryVariants.ContractEdgesGOFull.generate_operator(v, l, even_e)
+                                for v in range(18) for l in range(maxl+1)]
+        op_list = op_list + [OrdinaryVariants.ContractEdgesGOBridgeless.generate_operator(v, l, even_e)
+                                for v in range(18) for l in range(maxl+1)]
+        op_list = op_list + [OrdinaryVariants.ContractEdgesGOTriconnected.generate_operator(v, l, even_e)
+                                for v in range(18) for l in range(maxl+1)]
+
+    sumvsf = GraphVectorSpace.SumVectorSpace(vs_listf)
+    sumvsa = GraphVectorSpace.SumVectorSpace(vs_lista)
+    sumvs = GraphVectorSpace.SumVectorSpace(vs_lista + vs_listf)
+
+    allop = GraphOperator.OperatorMatrixCollection(sumvs, op_list)
+
+    print("Building full vector spaces.")
+    sumvsf.build_basis(n_jobs=nr_jobs)
+    print("Building other vector spaces.")
+    sumvsa.build_basis(n_jobs=nr_jobs)
+
+    # sumvs.build_basis(n_jobs=nr_jobs)
+    print("Building matrices.")
+    allop.build_matrix(n_jobs=nr_jobs)
+
+    print("Finished computing hairy matrices.")
+
+    print("computing ranks")
+    allop.compute_rank(linbox="mod", n_jobs=nr_jobs)
+    print("Finished")
