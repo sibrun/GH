27c11028066322341bb126e923405f598c446eeb
thomas <t.willwacher@gmail.com>
2025-03-11 14:35:17 +0100

    ...

--- source/TestOrdinaryVariants6.py
+++ source/TestOrdinaryVariants6.py
@@ -9,6 +9,7 @@ for v in range(20):
     vv = V.get_dimension()
     ww=W.get_dimension()
     uu=U.get_dimension()
-    print(l,v,":",vv,ww,uu)
+    # print(l,v,":",vv,ww,uu)
     if vv != 0:
-        print("Percentage:",100*ww/vv, 100*uu/vv)
+        # print("&", v, "&" , vv, "&", uu, "&", "Percentage:",100*ww/vv, 100*uu/vv)
+        print(F"& {v} & {vv} & {uu} ({100*uu//vv}%) & {ww} ({100*ww//vv}%) \\\\")
