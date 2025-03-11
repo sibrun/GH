0fe9de6139d12b798cc614fdf6584b8444e21780
thomas <t.willwacher@gmail.com>
2025-03-11 13:37:43 +0100

    ...

--- source/TestOrdinaryVariants6.py
+++ source/TestOrdinaryVariants6.py
@@ -6,4 +6,7 @@ for v in range(20):
     V = OrdinaryVariants.OrdinaryGVSFull(v,l,False)
     W = OrdinaryVariants.OrdinaryGVSTriconnected(v,l,False)
     U = OrdinaryGraphComplex.OrdinaryGVS(v,l,False)
-    print(l,v,":",V.get_dimension(),W.get_dimension(), U.get_dimension())
+    vv = V.get_dimension()
+    ww=W.get_dimension()
+    uu=U.get_dimension()
+    print(l,v,":",vv,ww,uu,100*ww/vv, 100*uu/vv)
