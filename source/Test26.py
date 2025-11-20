e30dd6fab23b11e372b13c4993d5d7a9320671e1
wilthoma <t.willwacher@gmail.com>
2025-11-20 20:57:49 +0100

    ....

--- source/Test26.py
+++ source/Test26.py
@@ -57,7 +57,16 @@ def basis_filter(V):
 def filter_cols(M, fil):
     cols = [i for i, ok in enumerate(fil) if ok]
     return M[:, cols]
+def filter_rows(M, fil):
+    rows = [i for i, ok in enumerate(fil) if ok]
+    return M[rows, :]
+def filter_both(M, fil_rows, fil_cols):
+    rows = [i for i, ok in enumerate(fil_rows) if ok]
+    cols = [i for i, ok in enumerate(fil_cols) if ok]
+    return M[rows, cols]
 
+def filter_from_sumvs(V, filterfunc):
+    return [filterfunc(g, VV) for VV in V.get_vs_list() for g in VV.get_basis() ]
 
 def cohom_formatted_forested_top(D1, D2, Dc2, filterfunc):
     vs = D1.get_domain()
@@ -67,6 +76,7 @@ def cohom_formatted_forested_top(D1, D2, Dc2, filterfunc):
         return "?"
     
     vs_fil = [filterfunc(g, vs) for g in vs.get_basis()]
+    out1_fil = filter_from_sumvs(D1.get_target(), filterfunc)
 
     # d = vs.get_dimension()
     d = sum( 1 for ok in vs_fil if ok)
@@ -79,6 +89,7 @@ def cohom_formatted_forested_top(D1, D2, Dc2, filterfunc):
 
     vs2 = D2.get_domain()
     vs2_fil = [filterfunc(g, vs2) for g in vs2.get_basis()]
+    out2_fil = filter_from_sumvs(D2.get_target(), filterfunc)
     d2 = sum( 1 for ok in vs2_fil if ok)
     
     if Dc2.is_valid():
@@ -93,20 +104,21 @@ def cohom_formatted_forested_top(D1, D2, Dc2, filterfunc):
 
     if D1.is_valid():
         A1 = D1.get_matrix()
-        A1_fil = filter_cols(A1, vs_fil)
+        A1_fil = filter_both(A1, out1_fil, vs_fil)
+        # A1_fil = filter_cols(A1, vs_fil)
         r1 = A1_fil.rank()
         # print("A1", A1.nrows(), A1.ncols(), r1)
         # print("A1_fil", A1_fil.nrows(), A1_fil.ncols(), r1)
 
     if D2.is_valid():
         A2 = D2.get_matrix()
-        A2_fil = filter_cols(A2, vs2_fil)
+        A2_fil = filter_both(A2, out2_fil, vs2_fil)
         r2 = A2_fil.rank()
         # print("A2", A2.nrows(), A2.ncols(), r2)
         # print("A2_fil", A2_fil.nrows(), A2_fil.ncols(), r2)
 
     # exact or not?
-    r_str = f"({d}+{rc2}-{r1}-{r2},{d},{d2})"
+    r_str = "" #f"({d}+{rc2}-{r1}-{r2},{d},{d2})"
 
     # iso string
     cohomdim = d+rc2-r1-r2
@@ -130,7 +142,7 @@ def create_forested_top_cohom_table(l_range, m_range, even_edges, filterfunc):
                 ) for m in m_range])
 
 
-create_forested_top_cohom_table(range(2,5), range(0,5), False, is_admissible)
+create_forested_top_cohom_table(range(2,6), range(0,10), False, is_admissible)
 
 # xxx = ForestedGraphComplex.ContractUnmarkTopBiOM.generate_operator(
 #                         2, 3, 2, False)
