e9b855a36595582464c24b180706383ba774ec9c
wilthoma <t.willwacher@gmail.com>
2025-12-25 20:39:08 +0100

    ..

--- source/test27.py
+++ source/test27.py
@@ -1,19 +1,17 @@
 from sage.all import *
 import OrdinaryGraphComplex
+import random
 
 
-op = OrdinaryGraphComplex.ContractEdgesGO.generate_operator(18,10,False)
-# op = OrdinaryGraphComplex.ContractEdgesGO.generate_operator(10,6,False)
-
+op = OrdinaryGraphComplex.ContractEdgesGO.generate_operator(13,8,False)
 D = op.get_matrix()
-
-
-
-# V = D.right_kernel()
-
-# print(V)
-# p = 1009  # 4-digit prime
-p = 100003  # 6-digit prime
-D_finite = D.change_ring(GF(p))
-V_finite = D_finite.right_kernel()
-print(V_finite)
+if D.nrows() > D.ncols():
+    D = D.transpose()
+r = D.rank()
+# select r rows at random
+rows = random.sample(range(D.nrows()), min(r+0, D.nrows()))
+D_sub = D[rows, :]
+r_sub = D_sub.rank()
+print("Size of D:", D.nrows(), "x", D.ncols())
+print("Original rank:", r)
+print("Submatrix rank:", r_sub)
